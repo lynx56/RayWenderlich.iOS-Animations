3128e8062c6d71397e12fb1e9dfe786c8549c035
Gulnaz <666lynx666@mail.ru>
2019-02-27 22:16:36 +0300

    Replication animations. Add initial layer

--- Iris assistant/Iris/ViewController.swift
+++ Iris assistant/Iris/ViewController.swift
@@ -24,38 +24,81 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-  
-  @IBOutlet weak var meterLabel: UILabel!
-  @IBOutlet weak var speakButton: UIButton!
-  
-  let monitor = MicMonitor()
-  let assistant = Assistant()
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
     
+    @IBOutlet weak var meterLabel: UILabel!
+    @IBOutlet weak var speakButton: UIButton!
     
-  }
-  
-  @IBAction func actionStartMonitoring(_ sender: AnyObject) {
+    let monitor = MicMonitor()
+    let assistant = Assistant()
     
-  }
-  
-  @IBAction func actionEndMonitoring(_ sender: AnyObject) {
+    let replicator = CAReplicatorLayer()
+    let dot = CALayer()
+    let dotLength: CGFloat = 6
+    let dotOffset: CGFloat = 8
     
-    //speak after 1 second
-    delay(seconds: 1.0) {
-      self.startSpeaking()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        replicator.frame = view.bounds
+        view.layer.addSublayer(replicator)
+        dot.frame = CGRect(x: replicator.frame.width - dotLength,
+                           y: replicator.position.y,
+                           width: dotLength,
+                           height: dotLength)
+        dot.backgroundColor = UIColor.lightGray.cgColor
+        dot.borderColor = UIColor(white: 1, alpha: 1).cgColor
+        dot.borderWidth = 0.5
+        dot.cornerRadius = 1.5
+        replicator.addSublayer(dot)
+        replicator.instanceCount = Int(view.frame.width / dotOffset)
+        replicator.instanceTransform = CATransform3DMakeTranslation(-dotOffset, 0.0, 0.0)
+        dot.add(CABasicAnimation.makeVerticalMove(from: dot.position.y,
+                                                  to: dot.position.y - 50,
+                                                  duration: 1,
+                                                  repeatCount: 10),
+                forKey: nil)
+        replicator.instanceDelay = 0.02
     }
-  }
-  
-  func startSpeaking() {
-    print("speak back")
     
+    @IBAction func actionStartMonitoring(_ sender: AnyObject) {
+        
+    }
     
-  }
-  
-  func endSpeaking() {
+    @IBAction func actionEndMonitoring(_ sender: AnyObject) {
+        
+        //speak after 1 second
+        delay(seconds: 1.0) {
+            self.startSpeaking()
+        }
+    }
     
-  }
+    func startSpeaking() {
+        print("speak back")
+        let scale = CABasicAnimation.make3DScale(x: 1.4, y: 15, z: 1.0, duration: 0.33)
+        dot.add(scale, forKey: nil)
+    }
+    
+    func endSpeaking() {
+        
+    }
+}
+
+extension CABasicAnimation {
+    static func makeVerticalMove(from: CGFloat, to: CGFloat, duration: CFTimeInterval, repeatCount: Float = 1) -> CABasicAnimation {
+        let move = CABasicAnimation(keyPath: "position.y")
+        move.fromValue = from
+        move.toValue = to
+        move.duration = duration
+        move.repeatCount = repeatCount
+        return move
+    }
+    static func make3DScale(x: CGFloat, y: CGFloat, z: CGFloat, duration: CFTimeInterval) -> CABasicAnimation{
+        let scale = CABasicAnimation(keyPath: "transform")
+        scale.fromValue = NSValue(caTransform3D: CATransform3DIdentity)
+        scale.toValue = NSValue(caTransform3D: CATransform3DMakeScale(x, y, z))
+        scale.duration = duration
+        scale.repeatCount = .infinity
+        scale.autoreverses = true
+        scale.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        return scale
+    }
 }
